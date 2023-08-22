ddf730b5096663095a430d7e86872f56750ae7de
isfan4ik <isfandiyorprogrammer@gmail.com>
2023-08-22 06:13:46 -0700

    lesson2

---
+++ main.js
@@ -0,0 +1,169 @@
+// 1.
+// let k = 10;
+// let n = 4;
+
+// for (let i = 0; i <= n; i++) {
+//    console.log(k);
+// }
+
+
+// 2.
+// let a = 1;
+
+// let b = 20;
+
+// for (let i = a; i <= b; i++) {
+//    if (i % 2 == 0) {
+//       console.log();
+//    } else {
+      
+//    }
+// }
+
+
+
+
+// numbers(20 , 6)
+
+
+// 3-masala
+// function numbers(a,b){
+//     for(let i = a-1;i>b ; i--){
+//         console.log(i);
+//     }
+// }
+// numbers(20 , 6)
+
+
+// 4.
+
+// let  =  10000;
+
+// for (let i = 0; i < 10; i++) {
+//    console.log(10000);   
+// }
+
+// 5.
+
+// let  a =  10000;
+
+// for (let i = 0.1 ; a - 0.1; i++) {
+//    console.log(a);   
+// }
+
+// .6
+
+// let a = 6;
+// let b = 5;
+// let c = 4;
+
+// for (let i = 0; 4 < 6 ; i++) {
+//    console.log(a,c);
+// }
+
+// .7
+
+// let a = 7;
+// let b =6;
+
+// let Sum = 0
+// function sum(a,b){
+//     for(let i = a; i<b;i++){
+//         Sum+=i   
+//     }
+//     console.log(Sum)
+// }
+// sum(1,4)
+
+
+// .8
+// let a = 14;
+// let b = 19;
+// let c = 10;
+// let d = 4;
+
+// function musbat(a,b,c,d) {
+//    for (let i = a; i < b ; i++) {
+//       console.log(musbat);
+//    }
+// }
+
+// .9
+
+// let = Sum
+
+// function Sum(a,b,c) {
+//    for (let i = a; i < b; i++) {
+//       let pow = Math.pow(2,i)
+//       Sum+pow
+//    }
+//    console.log(Sum);
+// }
+// Sum = (5,3,7)
+
+// .10
+// tushunmadim
+
+// .11
+
+// let n = 10;
+
+// for (let i = 1; i < n; i++) {
+//     if (i % 2 != 0) console.log(i);
+
+
+// .12
+// let n = 10
+// for (let i = 1; i < n; i++) {
+//     if (i % 2 == 0) console.log(i);
+
+// .13
+
+// let n = 17;
+// for (let i = 0; i < n; i++) {
+//    if (i % 12 == 2) {
+//       console.log(i);
+//    } else {
+//       console.log(n);
+//    }
+// }
+
+// .14
+
+// let n = 20;
+
+// for (let i = 13; i < n; i++) {
+//    console.log(i);
+//    if (i + n) {
+//       console.log(n);
+//    } else {
+//       console.log(i);
+//    }
+// }
+
+// .15
+
+// let n =10;
+
+// function n(n) {
+//    for (let i = 0; i < n; i++) {
+//       console.log(n);
+//    }
+// }
+
+// .16
+
+// let n = 5;
+// function betwen(n) {
+//    for (let i = 0; i == n; i++) {
+//       console.log(i);
+//    }
+// }
+
+// .17
+
+// let n = 9;
+
+// for (let i = 2; i < n; i++) {
+//    console.log(i);
+// }
